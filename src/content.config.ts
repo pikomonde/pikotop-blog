32c03d4b730584922e1f64a78bef3dff9ad7913f
pikomonde <32364823+pikomonde@users.noreply.github.com>
2026-06-12 14:30:56 +0700

    feat: add optional series order, fallback to pubdate

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,6 +16,7 @@ const blog = defineCollection({
 			heroImage: z.optional(image()),
 			tagTopics: z.array(z.string()).default(['General']), // Example: ["Astro", "Web Dev", "Engineering Excellence"]
 			tagSeries: z.optional(z.array(z.string())), // Editorial convention: max 1 series per article. Example: ["Building PikoMo Blog", "Intro to Programming"]
+			seriesOrder: z.record(z.string(), z.number()).optional(),
 			lang: z.enum(['id', 'en']).default('en'),
 			publishedOn: z.record(z.string(), z.string()).optional(), // Example: { medium: "url", devto: "url", ... }
 			draft: z.boolean().default(false),
